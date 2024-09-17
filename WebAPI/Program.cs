aa14d8d4804a9d5ed1fb7a951cab0c799c7d39f9
BerkaySahin26 <berkay.sahin26TR@gmail.com>
2024-09-17 20:26:49 +0300

    API New

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,10 +1,7 @@
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
-using Business.Abstract;
-using Business.Concrete;
+using Autofac;
 using Business.DependencyResolvers.Autofac;
-using DataAccess.Abstract;
-using DataAccess.Concrete.EntityFramework;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,10 +18,6 @@ builder.Host.ConfigureContainer<ContainerBuilder>(builder =>
 
 // Add services to the container
 builder.Services.AddControllers();
-// Uncomment and use the following lines if you want to register services manually
-
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
